b82a08f3e0d4a3854f798f76d603ec28b1465850
tesw yew isal <278153+cnmade@users.noreply.github.com>
2021-07-20 18:56:19 +0800

    初始化代码仓库

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using bsmi_phoenix_mail.area.about.view;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bsmi_phoenix_mail
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AboutWindow aw = new AboutWindow();
+            aw.Show();
+        }
+    }
+}
